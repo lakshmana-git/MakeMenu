93ab44da32ecda6bc866bc3a9a3e7caa4dbc0545
lakshmana-git <devlakshmanarao@gmail.com>
2023-11-03 22:59:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,10 +19,10 @@ const app = express()
 
 app.use(express.static(path.join(__dirname,'client')))
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client','index.html'));
 });
 
 app.use(express.json())
