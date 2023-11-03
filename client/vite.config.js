4f04e8f4123a16cd32bb68e92fb9260fc7cb3886
lakshmana-git <devlakshmanarao@gmail.com>
2023-11-03 22:52:11 +0530

    pre

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://localhost:3000",
+        target:"http://make-menu.onrender.com/",
         secure:false,
       },
     },
