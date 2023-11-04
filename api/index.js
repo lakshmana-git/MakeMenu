e5552c67574d6c3834bb6191b49d6aea87cfb42c
lakshmana-git <devlakshmanarao@gmail.com>
2023-11-04 09:47:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,14 +18,15 @@ const __dirname = path.resolve()
 const app = express()
 app.use(express.static(path.join(__dirname,'/client/dist')))
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})
-
+app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  });
 app.use(express.json())
 
 
 app.use(cookieParser())
+
+
 
 
 mongoose.connect(process.env.DB_URL).then(()=>{
